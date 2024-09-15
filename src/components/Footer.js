d53d1c435b298d8407cbbaf568ce167fb2027b63
Vivek Dhiman <114228863+88VwV88@users.noreply.github.com>
2024-09-15 14:16:26 +0530

    Update links in the Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="text-gray-600 body-font   ">
       <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap  flex-col">
         <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-          <a className="flex ml-10 title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <a href="#" className="flex ml-10 title-font font-medium items-center md:justify-start justify-center text-gray-900">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -55,7 +55,7 @@ const Footer = () => {
             </a>
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-            <a className="text-gray-500">
+            <a href="#" className="text-gray-500">
               <svg
                 fill="currentColor"
                 strokeLinecap="round"
@@ -67,7 +67,7 @@ const Footer = () => {
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-500">
+            <a href="#" className="ml-3 text-gray-500">
               <svg
                 fill="currentColor"
                 strokeLinecap="round"
@@ -79,7 +79,7 @@ const Footer = () => {
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-500">
+            <a href="#" className="ml-3 text-gray-500">
               <svg
                 fill="none"
                 stroke="currentColor"
@@ -93,7 +93,7 @@ const Footer = () => {
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
             </a>
-            <a className="ml-3 text-gray-500">
+            <a href="#" className="ml-3 text-gray-500">
               <svg
                 fill="currentColor"
                 stroke="currentColor"
